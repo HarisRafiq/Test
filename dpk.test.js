e113fa20e63593cc2084d8e9459eac56e8f9049d
Haris Rafiq <mrafi0015@gmail.com>
2022-08-31 15:18:37 +0000

    Update dpk.test.js

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,16 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+  it("Returns the literal '1234' when given no obj with partitionKey 1234", () => {
+    const trivialKey = deterministicPartitionKey({partitionKey:'1234'});
+    expect(trivialKey).toBe("1234");
+  });
+  it("Returns the literal 'string' when given type number as partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({partitionKey:1234});
+    expect(typeof trivialKey).toBe("string")
+  });
+  it("Returns the literal 'string' when given type number as partitionKey", () => {
+    const trivialKey = deterministicPartitionKey({partitionKey:1234});
+    expect(typeof trivialKey).toBe("string")
+  });
 });
